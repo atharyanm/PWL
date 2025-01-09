7f20c7d6c11d077b600aa422287ec3c6d622485c
atharyanm <r.atha.m025@gmail.com>
2025-01-10 04:35:30 +0700

    modified:   Tugas2/index.php
    Untracked files:
    	UTS/

--- Tugas2/index.php
+++ Tugas2/index.php
@@ -75,7 +75,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Sistem</title>
+    <title>Login | Sistem Akademik</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/index.css">
